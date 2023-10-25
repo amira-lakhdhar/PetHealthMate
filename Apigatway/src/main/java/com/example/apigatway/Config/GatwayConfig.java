27e3808f358180a493359c4bd5960fdc087f09e0
khayat asma <asma.khayat@esprit.tn>
2023-10-25 11:38:30 +0200

    eureka & api gateway en ecoute

--- Apigatway/src/main/java/com/example/apigatway/Config/GatwayConfig.java
+++ Apigatway/src/main/java/com/example/apigatway/Config/GatwayConfig.java
@@ -13,8 +13,9 @@ public class GatwayConfig {
     @Bean
     public RouteLocator routes(RouteLocatorBuilder builder) {
         return builder.routes()
-                .route("amiraweb", r -> r.path("/pethealth/amiraweb/**").uri("lb://amiraweb"))
                 .route("amiraweb", r -> r.path("/pethealth/userms/**").uri("lb://userms"))
+                .route("amiraweb", r -> r.path("/pethealth/adoption-service/**").uri("lb://adoption-service"))
+                .route("amiraweb", r -> r.path("/pethealth/amiraweb/**").uri("lb://amiraweb"))
                 .route("discovery-server", r -> r.path("/eureka/web").filters(f -> f.setPath("/")).uri("http://localhost:8761"))
                 .route("discovery-server-static", r -> r.path("/eureka/**") .uri("http://localhost:8761"))
                 .build();
