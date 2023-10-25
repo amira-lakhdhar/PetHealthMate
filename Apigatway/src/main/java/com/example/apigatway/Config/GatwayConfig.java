65763f18b93e58fc6871fab7e8eb64413c343b2c
khayat asma <asma.khayat@esprit.tn>
2023-10-25 15:03:29 +0200

    docker compose for my sql & adoption service

--- Apigatway/src/main/java/com/example/apigatway/Config/GatwayConfig.java
+++ Apigatway/src/main/java/com/example/apigatway/Config/GatwayConfig.java
@@ -13,8 +13,8 @@ public class GatwayConfig {
     @Bean
     public RouteLocator routes(RouteLocatorBuilder builder) {
         return builder.routes()
-                .route("amiraweb", r -> r.path("/pethealth/userms/**").uri("lb://userms"))
-                .route("amiraweb", r -> r.path("/pethealth/adoption-service/**").uri("lb://adoption-service"))
+                .route("userms", r -> r.path("/pethealth/userms/**").uri("lb://userms"))
+                .route("adoption-service", r -> r.path("/pethealth/adoption-service/**").uri("lb://adoption-service"))
                 .route("amiraweb", r -> r.path("/pethealth/amiraweb/**").uri("lb://amiraweb"))
                 .route("discovery-server", r -> r.path("/eureka/web").filters(f -> f.setPath("/")).uri("http://localhost:8761"))
                 .route("discovery-server-static", r -> r.path("/eureka/**") .uri("http://localhost:8761"))
